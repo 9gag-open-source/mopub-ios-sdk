ac9ae1cb69d1e57f0c9210a98e636660c0934a87
hei <hei@9gag.com>
2017-03-02 16:38:07 +0800

    Added autorelease pool

--- MoPubSDK/Native Ads/Internal/MPNativeAdRendererImageHandler.m
+++ MoPubSDK/Native Ads/Internal/MPNativeAdRendererImageHandler.m
@@ -53,34 +53,36 @@
             // assignment to a UIImageView.
             
             // Fix memory issue for decompress image
-            CGImageRef imageRef = image.CGImage;
-            // System only supports RGB, set explicitly and prevent context error
-            // if the downloaded image is not the supported format
-            CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-            
-            CGContextRef context = CGBitmapContextCreate(NULL,
-                                                         CGImageGetWidth(imageRef),
-                                                         CGImageGetHeight(imageRef),
-                                                         8,
-                                                         // width * 4 will be enough because are in ARGB format, don't read from the image
-                                                         CGImageGetWidth(imageRef) * 4,
-                                                         colorSpace,
-                                                         // kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Little
-                                                         // makes system don't need to do extra conversion when displayed.
-                                                         kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Little);
-            CGColorSpaceRelease(colorSpace);
-            
-            if (!context) {
-                return;
+            @autoreleasepool {
+                CGImageRef imageRef = image.CGImage;
+                // System only supports RGB, set explicitly and prevent context error
+                // if the downloaded image is not the supported format
+                CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+                
+                CGContextRef context = CGBitmapContextCreate(NULL,
+                                                             CGImageGetWidth(imageRef),
+                                                             CGImageGetHeight(imageRef),
+                                                             8,
+                                                             // width * 4 will be enough because are in ARGB format, don't read from the image
+                                                             CGImageGetWidth(imageRef) * 4,
+                                                             colorSpace,
+                                                             // kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Little
+                                                             // makes system don't need to do extra conversion when displayed.
+                                                             kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Little);
+                CGColorSpaceRelease(colorSpace);
+                
+                if (!context) {
+                    return;
+                }
+                CGRect rect = (CGRect){CGPointZero, CGImageGetWidth(imageRef), CGImageGetHeight(imageRef)};
+                CGContextDrawImage(context, rect, imageRef);
+                CGImageRef decompressedImageRef = CGBitmapContextCreateImage(context);
+                CGContextRelease(context);
+                UIImage *decompressedImage = [[UIImage alloc] initWithCGImage:decompressedImageRef];
+                CGImageRelease(decompressedImageRef);
+                
+                [self safeMainQueueSetImage:decompressedImage intoImageView:imageView];
             }
-            CGRect rect = (CGRect){CGPointZero, CGImageGetWidth(imageRef), CGImageGetHeight(imageRef)};
-            CGContextDrawImage(context, rect, imageRef);
-            CGImageRef decompressedImageRef = CGBitmapContextCreateImage(context);
-            CGContextRelease(context);
-            UIImage *decompressedImage = [[UIImage alloc] initWithCGImage:decompressedImageRef];
-            CGImageRelease(decompressedImageRef);
-            
-            [self safeMainQueueSetImage:decompressedImage intoImageView:imageView];
         } else if (imageURL) {
             MPLogDebug(@"Cache miss on %@. Re-downloading...", imageURL);
 
